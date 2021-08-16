02202bc6b1c97a7b16e1d9d5fed667fb26ded99c
andrey <andreykaxb@gmail.com>
2021-08-16 15:26:38 +0400

    new features in file

--- core.py
+++ core.py
@@ -1,5 +1,5 @@
 import streamlit as st
-
+isAuth = False
 def login(a):
     if a == '1':
         return True
@@ -11,8 +11,13 @@ st.write('''
 # Создание отчета ДДС
 ''')
 
-passwd = st.text_input('Пароль')
-st.button('Войти')
+if not isAuth:
+    passwd = st.text_input('Пароль')
+    st.button('Войти')
 
-if login(passwd):
-    st.text('Успешно')
+    if login(passwd):
+        st.text('Успешно')
+        isAuth = True
+
+if isAuth:
+    st.header('Создание отчета')
