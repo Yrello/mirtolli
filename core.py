e3e88d254d954d304060d0ea04ad2559907e501c
andrey <andreykaxb@gmail.com>
2021-08-16 15:19:53 +0400

    new features in file

--- core.py
+++ core.py
@@ -1,6 +1,18 @@
 import streamlit as st
+
+def login(a):
+    if a == '1':
+        return True
+    else:
+        return False
+
+
 st.write('''
 # Создание отчета ДДС
 ''')
+
+passwd = st.text_input()
 st.button('Войти')
-st.button('Выйти')
+
+if login(passwd):
+    st.text('Успешно')
